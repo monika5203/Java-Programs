b08610c2dc99ed9e725c828763f34f01f853eb2c
Monika Sharma <monikasharma@MAC171-MS-LTR025.local>
2023-10-11 11:10:30 +0530

    changesss

--- src/StringOccurence.java
+++ src/StringOccurence.java
@@ -4,7 +4,7 @@ public class StringOccurence {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String Original="monika sharma";
+		String Original="monika sharmaa";
 		char ch= 'a';
 		int count=0;
 		for(int i=0; i<Original.length(); i++)
